e529b2d9568989dfbf49b4d752819d8bc8b183b3
Vasiliy <Freeneos@icloud.com>
2022-04-22 17:31:20 +0300

    add harder

--- Headers/TestSSH-Bridging-Header.h
+++ Headers/TestSSH-Bridging-Header.h
@@ -1,2 +1,8 @@
 #import </usr/local/include/libssh/libssh.h>
 #import </usr/local/include/libssh/libssh_version.h>
+#import </usr/local/include/libssh/callbacks.h>
+#import </usr/local/include/libssh/legacy.h>
+#import </usr/local/include/libssh/libsshpp.hpp>
+#import </usr/local/include/libssh/server.h>
+#import </usr/local/include/libssh/sftp.h>
+#import </usr/local/include/libssh/ssh2.h>
